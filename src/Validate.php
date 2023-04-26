bab5e1e94176ad93f335b6c9d36ac3524e6209a0
@Jc-2600 <juan.lopez1@utp.edu.co>
2023-04-26 16:06:09 -0500

    Add more Validate-test for url and password

--- src/Validate.php
+++ src/Validate.php
@@ -7,4 +7,12 @@ class Validate{
     public static function email($value){
         return (bool)filter_var($value, FILTER_VALIDATE_EMAIL);
     }
+    public static function url($value){
+        return (bool)filter_var($value, FILTER_VALIDATE_URL);
+    }
+
+    public static function password($value)
+    {
+        return (bool)preg_match('/^[0-9A-Za-z\.\-]{6,9}$/', $value);
+    }
 }
--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -12,9 +12,25 @@ class ValidateTest extends TestCase{
         
         $email = Validate::email('i@@@rimosoft.com');
         $this->assertFalse($email); 
+    }
+    public function test_url(){
 
-    
+        $url = Validate::url('https://platzi.com');
+        $this->assertTrue($url);
+        
+        $url = Validate::url('platzi.com');
+        $this->assertFalse($url); 
+    }
+    public function test_password(){
+
+        $pass = Validate::password('pedro.A12');
+        $this->assertTrue($pass); 
+
+        $pass = Validate::password('Jo.ge');
+        $this->assertFalse($pass);
 
     }
+
+
 
 }
